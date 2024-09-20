2a306995f9c2999231d6fabcddc41f1e8e941a15
abhishek-end <abhishekyadav49534@gmail.com>
2024-09-20 23:38:15 +0530

    Server with express is created and connection to the mongoDB Suceesfull

---
+++ api/DB/configdb.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const mongoConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database Connected");
+  } catch (error) {
+    console.log("Error Connecting Database", error);
+    process.exit(1);
+  }
+};
+module.exports = mongoConnect;
--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
+const connectDB = require("./DB/configdb");
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send(`Server is Running on PORT ${port}`);
